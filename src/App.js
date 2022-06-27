43092802fe1935d197ec09b041043107c1cf01d2
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-06-27 11:12:43 +0200

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Container>
+        <Row>
+          <Col>
+            PARALAX
+          </Col>
+          <Col>
+            PARALAX 2
+          </Col>
+        </Row>
+      </Container>
+      <Button>Nasus</Button>
+      <br/>
+      About Me<br/>
+      Projects<br/>
+      Education<br/>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="navbar">
+        Navbar
+      </div>
+
+    );
+  }
+}
+
+export default Navbar;

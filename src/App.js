a906b72159d28fa05f032a12296d04934dbefe7a
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-06-27 15:43:16 +0200

    Simple navbar

--- src/App.js
+++ src/App.js
@@ -6,28 +6,45 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Cookies from 'js-cookie';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Container>
-        <Row>
-          <Col>
-            PARALAX
-          </Col>
-          <Col>
-            PARALAX 2
-          </Col>
-        </Row>
-      </Container>
-      <Button>Nasus</Button>
-      <br/>
-      About Me<br/>
-      Projects<br/>
-      Education<br/>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: Cookies.get('language') || 'pl'
+    };
+    this.handleLanguageChange = this.handleLanguageChange.bind(this);
+  }
+
+  handleLanguageChange(event) {
+    if(this.state.language === 'pl') {
+      this.setState({
+        language: 'en'
+      });
+      Cookies.set('language', 'en');
+    } else{
+      this.setState({
+        language: 'pl'
+      });
+      Cookies.set('language', 'pl');
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar changeLanguage={this.handleLanguageChange} language={this.state.language}/>
+        Language: {this.state.language}
+        <section id="about">About Me</section>
+        <section id="projects">Projects</section>
+        <section id="experience">Education and experience</section>
+        <section id="contact">Contact</section>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-scroll';
+import {Row, Col} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+
+function Navitem(props){
+  return (
+      <Link className='et-hero-tab' activeClass="active" smooth spy to={props.section.id}>
+        {props.section.name[props.language]}
+      </Link>
+  )
+}
 
 class Navbar extends React.Component {
-  render() {
-    return (
-      <div className="navbar">
-        Navbar
-      </div>
+  constructor(props) {
+    super(props);
+    this.sections = [{id: 'about', name: {'pl': 'O mnie', 'en': 'About'}}, {id: 'projects', name: {'pl': 'Projekty', 'en': 'Projects'}}, {id: 'experience', name: {'pl': 'Edukacja i Doświadczenie', 'en': 'Education & Experience'}}, {id: 'contact', name: {'pl': 'Kontakt', 'en': 'Contact'}}];
+  }
 
+  render() {
+    let navItems = this.sections.map(section => {
+      return <Navitem section={section} language={this.props.language} key={section['id']} />
+    });
+    return (
+      <section class="et-hero-tabs container-fluid">
+      <h1>STICKY SLIDER NAV</h1>
+      <h3>Sliding content with sticky tab nav</h3>
+      <div class="et-hero-tabs-container">
+        {navItems}
+        <Button onClick={this.props.changeLanguage}>
+          {this.props.language === 'pl' ? 'Polski' : 'English'}
+        </Button>
+        <span class="et-hero-tab-slider"></span>
+      </div>
+    </section>
     );
   }
 }

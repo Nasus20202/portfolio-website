ea57f7b65d31d41851dc2ef62cb50f82eb876524
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-07-30 15:48:00 +0200

    Change projects

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './Contact.css';
-import {Github, Envelope, Telephone, Discord, Linkedin} from 'react-bootstrap-icons'
+import {Github, Envelope, Discord, Linkedin} from 'react-bootstrap-icons'
 import {ParallaxProvider, ParallaxBanner} from 'react-scroll-parallax';
 
 function ContactLine(props){
@@ -26,7 +26,6 @@ class Experience extends React.Component
         this.contacts =[
             {name: {'pl': 'GitHub', 'en': 'GitHub'}, value: {'pl': 'Nasus20202', 'en': 'Nasus20202'}, link:'https://github.com/Nasus20202/', icon:<Github/>},
             {name: {'pl': 'Email', 'en': 'Email'}, value: {'pl': 'krzysztof.nasuta@wp.pl', 'en': 'krzysztof.nasuta@wp.pl'}, link:'mailto:krzysztof.nasuta@wp.pl', icon:<Envelope/>},
-            {name: {'pl': 'Telefon', 'en': 'Phone'}, value: {'pl': '786 17 12 00', 'en': '+48 786 17 12 00'}, link:'tel:+48786171200', icon:<Telephone/>},
             {name: {'pl': 'Discord', 'en': 'Discord'}, value: {'pl': 'Nasus#8143', 'en': 'Nasus#8143'}, link:'', icon:<Discord/>},
             {name: {'pl': 'LinkedIn', 'en': 'LinkedIn'}, value: {'pl': 'krzysztof-nasuta', 'en': 'krzysztof-nasuta'}, link:'https://www.linkedin.com/in/krzysztof-nasuta/', icon:<Linkedin/>}
         ];
